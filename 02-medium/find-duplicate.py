90f28ed003d8726c1392f45d8a7a659de6b62a15
Arya <aryagxr@gmail.com>
2024-05-24 20:31:10 -0500

    find duplicate

--- 02-medium/find-duplicate.py
+++ 02-medium/find-duplicate.py
@@ -2,7 +2,26 @@
 
 
 def findDuplicate(nums):
-    
+    nums2 = []
+    for i in range(len(nums)):
+        nums2.append(nums[i])
+        if i != len(nums)-1 and nums[i+1] in nums2:
+            return nums[i+1]
+
+
+
+
+def findDuplicate2(nums):
+    nums2=set()
+    for i in nums:
+        if i in nums2:
+            return i
+        nums2.add(i)
+
+
+   
+
+
 
 
 
