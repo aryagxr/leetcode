68eaaa4b1d5914099e0958312dd68dbc043a60fa
Arya <aryagxr@gmail.com>
2024-06-09 22:27:19 -0500

    subarray sum

--- 02-medium/cont-subarray-sum.py
+++ 02-medium/cont-subarray-sum.py
@@ -4,6 +4,14 @@ def checkSubarray(nums, k):
     # edge case
     if k == 0:
         return False # no good subarray
-    
+    # dictionary for index and rem
+    dict = {}
+    dict[0] = -1
+    pref_sum = 0
+    for i in range(len(nums)):
+        pref_sum += nums[i] #prefix sum
+        rem = pref_sum % k #remainder upto prefix sum
+
+
     
     
